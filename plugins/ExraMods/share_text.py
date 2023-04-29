335bf2b51bfe82033225b4f6b6f375875acf3c62
SANGRAM-MUSIC <120346630+SANGRAM-MUSIC@users.noreply.github.com>
2023-04-29 21:59:45 +0530

    Update share_text.py

--- plugins/ExraMods/share_text.py
+++ plugins/ExraMods/share_text.py
@@ -16,7 +16,7 @@ async def share_text(client, message):
         await message.reply_text(
             text=f"**Notice:**\n\n1. Reply Any Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",                
             reply_to_message_id=reply_id,               
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Support Chat", url=f"https://t.me/MKN_BOTZ_DISCUSSION_GROUP")]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("😁😁", url=f"https://t.me/THE_VIP_BOY")]])
             )                                                   
         return
     await message.reply_text(
