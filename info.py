f7fedaf0c8b7f86e57599b4e5bf1b466bbc45d23
SANGRAM-MUSIC <120346630+SANGRAM-MUSIC@users.noreply.github.com>
2023-04-30 08:19:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,7 +44,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', "")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 #maximum search result buttos count in number#
 MAX_RIST_BTNS = int(environ.get('MAX_RIST_BTNS', "10"))
